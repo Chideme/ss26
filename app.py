6857b6cbff04351d00d2b2227245d1e87c9a3dde
Chideme <kudakwashechideme@gmail.com>
2020-10-24 13:13:47 +0200

    fix session9

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ db.init_app(app)
 app.config.update(dict(
     DEBUG = False,
     MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = 587,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
+    MAIL_PORT = 465,
+    MAIL_USE_TLS = False,
+    MAIL_USE_SSL = True,
     MAIL_USERNAME = os.getenv("SENDER_MAIL"),
     MAIL_PASSWORD = os.getenv("EMAIL_PASSWORD"),
 ))
